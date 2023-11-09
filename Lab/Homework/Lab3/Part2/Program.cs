f558681b71f13c39d5c15a7fa9bbafcff6f3eb96
Qarty26 <116076301+Qarty26@users.noreply.github.com>
2023-11-10 00:51:05 +0200

    Add files via upload

--- Lab/Homework/Lab3/Part2/Program.cs
+++ Lab/Homework/Lab3/Part2/Program.cs
@@ -1,8 +1,13 @@
+using Lab3_DB.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<Lab3Context>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
