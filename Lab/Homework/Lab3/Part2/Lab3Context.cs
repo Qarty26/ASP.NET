93f85b5ca84b0cae81e33c035bd80bf2a4701734
Qarty26 <116076301+Qarty26@users.noreply.github.com>
2023-11-10 00:52:03 +0200

    Add files via upload

--- Lab/Homework/Lab3/Part2/Lab3Context.cs
+++ Lab/Homework/Lab3/Part2/Lab3Context.cs
@@ -1,15 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Lab3_DB.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Lab3_DB.Data
 {
     public class Lab3Context : DbContext
     {
-
+        public DbSet<Student> Students { get; set; }
         public Lab3Context(DbContextOptions<Lab3Context> options) : base(options) { }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-
-        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) { }
     }
 }
