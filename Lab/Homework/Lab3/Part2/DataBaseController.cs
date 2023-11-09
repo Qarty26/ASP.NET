761d055ca73230f72b4654297cf4138325ca81c0
Qarty26 <116076301+Qarty26@users.noreply.github.com>
2023-11-10 00:51:42 +0200

    Add files via upload

--- Lab/Homework/Lab3/Part2/DataBaseController.cs
+++ Lab/Homework/Lab3/Part2/DataBaseController.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Lab3_DB.Data;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Lab3_DB.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class DataBaseController : ControllerBase
+    public class DatabaseController : ControllerBase
     {
-        
+        private readonly Lab3Context _lab3Context;
+        public DatabaseController(Lab3Context lab3Context)
+        {
+            _lab3Context = lab3Context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _lab3Context.Students.ToListAsync());
+        }
     }
 }
