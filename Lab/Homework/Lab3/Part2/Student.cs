8da2bbc532fd34355a9dfbb142d20921a5b6436b
Qarty26 <116076301+Qarty26@users.noreply.github.com>
2023-11-10 00:52:20 +0200

    Add files via upload

--- Lab/Homework/Lab3/Part2/Student.cs
+++ Lab/Homework/Lab3/Part2/Student.cs
@@ -1,6 +1,12 @@
-﻿namespace Lab3_DB.Models
+﻿using Lab3_DB.Models.Base;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+
+namespace Lab3_DB.Models
 {
-    public class Student
+    public class Student: BaseEntity
     {
+        public string? Name { get; set; }
+        public int Age { get; set; }
     }
 }
