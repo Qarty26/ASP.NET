0f9264c850dd5fc084703bc5c8c61136b0266c50
Qarty26 <116076301+Qarty26@users.noreply.github.com>
2023-12-06 12:35:50 +0200

    Add files via upload

---
+++ Project/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Roads.Models;
+
+namespace Roads.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public List<User> users = new List<User>();
+
+
+        [HttpGet]
+        public List<User> Get()
+        {
+            return users;
+        }
+
+        [HttpPost]
+        public List<User> Add(User user)
+        {
+            users.Add(user);
+            return users;
+        }
+
+        [HttpGet("sortByXp")]
+        public List<User> OrderByXp()
+        {
+            users = users.OrderBy(x => x.Experience).ToList();
+            return users;
+        }
+
+
+        [HttpGet("sortByNoTracks")]
+        public List<User> OrderByNoTracks()
+        {
+            users = users.OrderBy(x => x.Tracks.Count).ToList();
+            return users;
+        }
+    }
+}
