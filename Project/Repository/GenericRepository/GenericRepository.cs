2b9723869e0fd17bf3fa27f3e3ea688b7639851b
Qarty26 <116076301+Qarty26@users.noreply.github.com>
2023-12-15 22:00:59 +0200

    Add files via upload

---
+++ Project/Repository/GenericRepository/GenericRepository.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Roads.Data;
+using Roads.Models.Base;
+
+namespace Roads.Repository.GenericRepository
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : BaseEntity
+    {
+        protected readonly RoadsContext _roadsContext;
+        protected readonly DbSet<TEntity> _table;
+
+        public GenericRepository(RoadsContext roadsContext)
+        {
+            _roadsContext = roadsContext;
+            _table = _roadsContext.Set<TEntity>();
+        }
+
+        //get
+        public List<TEntity> GetAll()
+        {
+            return _table.AsNoTracking().ToList();
+        }
+        public async Task<List<TEntity>> GetAllAsync()
+        {
+            return await _table.AsNoTracking().ToListAsync();
+        }
+
+        //create
+
+        public void Create(TEntity entity)
+        {
+            _table.Add(entity);
+        }
+        public async Task CreateAsync(TEntity entity)
+        {
+            await _table.AddAsync(entity);
+        }
+
+        public void CreateRange(IEnumerable<TEntity> entities)
+        {
+            _table.AddRange(entities);
+        }
+        public async Task CreateRangeAsync(IEnumerable<TEntity> entities)
+        {
+            await _table.AddRangeAsync(entities);
+        }
+
+        //update
+
+        public void Update(TEntity entity)
+        {
+            _table.Update(entity);
+        }
+
+        public void UpdateRange(IEnumerable<TEntity> entities)
+        {
+            _table.UpdateRange(entities);
+        }
+
+        //delete
+
+        public void Delete(TEntity entity)
+        {
+            _table.Remove(entity);
+        }
+
+        public void DeleteRange(IEnumerable<TEntity> entities)
+        {
+            _table.RemoveRange(entities);
+        }
+
+        //find
+
+        public TEntity FindById(Guid id)
+        {
+            return _table.Find(id);
+        }
+
+        public async Task<TEntity> FindByIdAsync(Guid id)
+        {
+            return await _table.FindAsync(id);
+        }
+
+
+        //save
+        public bool Save()
+        {
+            return _roadsContext.SaveChanges() > 0;
+        }
+        public async Task<bool> SaveAsync()
+        {
+            return await _roadsContext.SaveChangesAsync() > 0;
+        }
+    }
+}
---
+++ Project/Repository/GenericRepository/IGenericRepository.cs
@@ -0,0 +1,37 @@
+﻿using Roads.Models.Base;
+
+namespace Roads.Repository.GenericRepository
+{
+    public interface IGenericRepository<TEntity> where TEntity : BaseEntity
+    {
+        //get all
+        List<TEntity> GetAll();
+        Task<List<TEntity>> GetAllAsync();
+
+        //create
+        void Create(TEntity entity);
+        Task CreateAsync(TEntity entity);
+        void CreateRange(IEnumerable<TEntity> entitites);
+        Task CreateRangeAsync(IEnumerable<TEntity> entitites);
+
+        //update
+        void Update(TEntity entity);
+        void UpdateRange(IEnumerable<TEntity> entitites);
+
+        //delete
+
+        void Delete(TEntity entity);
+        void DeleteRange(IEnumerable<TEntity> entitites);
+
+        //find
+
+        TEntity FindById(Guid id);
+        Task<TEntity> FindByIdAsync(Guid id);
+
+        //save
+
+        bool Save();
+        Task<bool> SaveAsync();
+
+    }
+}
