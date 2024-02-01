6d4e0ebfc997659d99c17ed60e68060c3f1d52f1
Qarty26 <116076301+Qarty26@users.noreply.github.com>
2024-02-01 14:09:03 +0200

    added create and delete endpoints

--- Project/Frontend/src/models/Car.ts
+++ Project/Frontend/src/models/Car.ts
@@ -1,7 +1,26 @@
 import { Generic } from './Generic'
+import axios from "axios";
 
 export class Car extends Generic {
     protected routeName: string = 'Car'
+
+    public createCar = async (payload : any) => {
+        return await axios.post(`${this.routeName}/create`,payload)
+            .then((response => response.data))
+            .catch((error => {
+                console.error(error.response);
+                throw error.response.data.title;
+            }))
+    }
+
+    public deleteCar = async (id: string) => {
+        return await axios.post(`${this.routeName}/delete/${id}`)
+            .then((response => response.data))
+            .catch((error => {
+                console.error(error.response);
+                throw error.response.data.title;
+            }))
+    }
 }
 
 export interface ICar {
--- Project/Frontend/src/models/User.ts
+++ Project/Frontend/src/models/User.ts
@@ -1,7 +1,26 @@
 import { Generic } from './Generic'
+import axios from "axios";
 
 export class User extends Generic {
-    protected routeName: string = "User";
+    protected routeName: string = "User"
+
+    public createUser = async (payload : any) => {
+        return await axios.post(`${this.routeName}/create`,payload)
+            .then((response => response.data))
+            .catch((error => {
+                console.error(error.response);
+                throw error.response.data.title;
+            }))
+    }
+
+    public deleteUser = async (id: string) => {
+        return await axios.post(`${this.routeName}/delete/${id}`)
+            .then((response => response.data))
+            .catch((error => {
+                console.error(error.response);
+                throw error.response.data.title;
+            }))
+    }
 }
 
 export interface IUser {
